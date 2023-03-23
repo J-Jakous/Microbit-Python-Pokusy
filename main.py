8573b53c551634f7dc88f883afe71b53a9c07f9c
J-Jakous <128708829+J-Jakous@users.noreply.github.com>
2023-03-23 14:02:56 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_forever():
+    for l in str("Popokatepetl").to_upper_case():
+        basic.show_string(l)
+
+#basic.forever(on_forever)
+
+basic.show_arrow(ArrowNames.NORTH_WEST)
---
+++ main.ts
@@ -0,0 +1,8 @@
+function on_forever() {
+    for (let l of ("" + "Popokatepetl").toUpperCase()) {
+        basic.showString(l)
+    }
+}
+
+// basic.forever(on_forever)
+basic.showArrow(ArrowNames.NorthWest)
