b4651604eec8bb874005c79506dcebf489f2c840
Jan Jakouš <128708829+J-Jakous@users.noreply.github.com>
2023-03-30 10:12:41 +0200

    Aktualizovat pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-def on_forever():
-    for l in str("Popokatepetl").to_upper_case():
-        basic.show_string(l)
+def on_button_pressed_a():
+    basic.show_string("")
 
-#basic.forever(on_forever)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
-basic.show_arrow(ArrowNames.NORTH_WEST)
+def on_button_pressed_b():
+    basic.clear_screen()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+img = images.create_big_image("""
+    . . . . .
+        . . . . .
+        . # # . .
+        . . . . .
+        . . . . .
+""")
--- main.ts
+++ main.ts
@@ -1,8 +1,63 @@
-function on_forever() {
-    for (let l of ("" + "Popokatepetl").toUpperCase()) {
-        basic.showString(l)
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    display_dice(randint(1, 6))
+})
+function display_dice (value: number) {
+    if (value == 1) {
+        images.createImage(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `).showImage(0)
+    }
+    if (value == 2) {
+        images.createImage(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `).showImage(0)
+    }
+    if (value == 3) {
+        images.createImage(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `).showImage(0)
+    }
+    if (value == 4) {
+        images.createImage(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `).showImage(0)
+    }
+    if (value == 5) {
+        images.createImage(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `).showImage(0)
+    }
+    if (value == 6) {
+        images.createImage(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `).showImage(0)
     }
 }
-
-// basic.forever(on_forever)
-basic.showArrow(ArrowNames.NorthWest)
